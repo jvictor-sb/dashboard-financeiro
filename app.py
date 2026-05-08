c28baa76405d505e86a8e8943f5a5813ca141e92
Victor <257781671+jvictor-sb@users.noreply.github.com>
2026-05-08 15:32:03 -0300

    feat(auth) Cadastro/login funcionando

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from auth.routes import auth
+from inicio.routes import inicio
 
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta'
 
-@app.route('/')
-def inicial():
-    return 'Hello World!'
-    
 app.register_blueprint(auth)
+app.register_blueprint(inicio)
 
 if __name__ == '__main__':
     app.run(debug=True)
