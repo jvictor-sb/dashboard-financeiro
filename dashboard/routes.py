47a073c60ce0809eaa73d6a628de7c686eb659ad
jvictor-santos <joaovictor2008.santos@gmail.com>
2026-06-04 10:42:42 -0300

    refactor(templates) Adiciona uso de herança no jinja2

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -6,4 +6,15 @@ dashboard = Blueprint('dashboard', __name__)
 @dashboard.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    dados_financeiros = {
+        "saldo_total": "R$ 3.450,00",
+        "receita_total": "R$ 5.000,00",
+        "despesas_totais": "R$ 1.550,00",
+        "qtd_transacao": 14
+    }
+    
+    return render_template(
+        'index.html', 
+        active_page='visao_geral', 
+        **dados_financeiros
+    )
